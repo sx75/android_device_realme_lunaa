faedfbc4f4a57def75acc411643d6a30fe557d4b
Quallenauge <Hamsi2k@freenet.de>
2023-05-16 20:19:52 +0200

    Revert "lunaa: Import sensors hal with als correction from oneplus sm8150-common"
    This reverts commit de171c42f0611d4a950a90712d0386591d2debdf.

--- device.mk
+++ device.mk
@@ -55,11 +55,6 @@ PRODUCT_PACKAGES += \
     OPlusSettingsProviderResTarget \
     OPlusSystemUIResTarget
 
-# Sensors
-PRODUCT_PACKAGES += \
-    als_correction_service.lunaa \
-    android.hardware.sensors@2.0-service.multihal.lunaa
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
