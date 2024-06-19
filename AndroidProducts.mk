d47abd8dd4d8a8481106e0e854be9a3ac4ed3124
SX <152792462+sx75@users.noreply.github.com>
2024-06-19 15:54:02 +0530

    lunaa : WELCOME DERP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lunaa.mk
+    $(LOCAL_DIR)/derp_lunaa.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lunaa-user \
-    lineage_lunaa-userdebug \
-    lineage_lunaa-eng
+    derp_lunaa-user \
+    derp_lunaa-userdebug \
+    derp_lunaa-eng
