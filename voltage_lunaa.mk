5659d00c98ccd932bc81874805000ed145486add
SX <neoroms69@gmail.com>
2024-08-29 08:16:19 +0530

    vos rebrand

--- voltage_lunaa.mk
+++ voltage_lunaa.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/lunaa/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_lunaa
 PRODUCT_DEVICE := lunaa
